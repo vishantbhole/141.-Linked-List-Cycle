12967e1d9a156907e8fe393f4228e4589f12cfbd
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-05-14 21:53:35 -0500

    solution

--- ListNode.py
+++ ListNode.py
@@ -3,3 +3,13 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+class Solution:
+    def hasCycle(self, head):
+        seen = []
+        curr = head
+        while curr:
+            if curr in seen:
+                return True
+            seen.append(curr)
+            curr = curr.next
+        return False
