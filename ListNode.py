bdf502035c2e58ee672ee36d018082ab918bb400
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-05-14 21:55:05 -0500

    create_linked_list

--- ListNode.py
+++ ListNode.py
@@ -13,3 +13,13 @@ class Solution:
             seen.append(curr)
             curr = curr.next
         return False
+def create_linked_list(values, pos):
+    if not values:
+        return None
+
+    nodes = [ListNode(val) for val in values]
+    for i in range(len(nodes) - 1):
+        nodes[i].next = nodes[i + 1]
+    if pos != -1:
+        nodes[-1].next = nodes[pos]
+    return nodes[0]
